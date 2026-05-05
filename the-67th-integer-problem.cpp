80a0db72e8f202c8ec3ee6d08d9775bd45bc4681
apnudpepey <ngdangtuandung@gmail.com>
2026-05-06 01:37:42 +1000

    2218A

---
+++ the-67th-integer-problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+int main() {
+    int t;
+    cin >> t;
+    REP(i, 0, t) {
+        int x;
+        cin >> x;
+        int y;
+        y = x;
+        cout << y << endl;
+    }
+    return 0;
+}
